d5aaa07c9e3053c481b8d2055a21371bc2ad632a
s4hil <s4hilp4rr4y@gmail.com>
2022-11-28 00:28:09 +0530

    initial

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import mysql from "mysql"
+import cors from 'cors'
+
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "node_crud"
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res)=>{
+    res.json("hello this is backend")
+})
+
+app.get("/users", (req, res)=>{
+    const q = "SELECT * FROM `users`";
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.post("/users", (req, res)=>{
+    const q = "INSERT INTO users (`name`, `email`) VALUES (?)"
+    const vals = [
+        req.body.name,
+        req.body.email
+    ];
+ 
+    db.query(q, [vals], (err, data)=>{
+        if(err) return res.json(err);
+        return res.json("user has been created")
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("Connected!!");
+})
